058449d00c41b2118b291335f75f466291b715f7
wassen <k-asayama@outlook.com>
2017-07-25 01:43:53 +0900

    feat: tasks of previous day

--- src/entry_point.py
+++ src/entry_point.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import sys
-# configへ
-nippo_runtime_path = vim.eval("g:nippo#runtime_path")
+from datetime import timedelta
+from datetime import date
+
+try:
+    nippo_runtime_path = vim.eval("g:nippo#runtime_path")
+except NameError:
+    nippo_runtime_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(nippo_runtime_path)
-from src.nippo import Nippo 
+from src.nippo import Nippo
+
+
 def nippo_main(arg="today"):
-    Nippo(arg).open()
+
+    def parse_arg(arg):
+        # tasks done this week
+        if "today".startswith(arg):
+            return date.today()
+        elif "yesterday".startswith(arg):
+            return date.today() + timedelta(days=-1)
+        else:
+            try:
+                return date.today() - timedelta(days=int(arg))
+            except ValueError:
+                print("invarid argument")
+
+    Nippo(parse_arg(arg)).open()
 
--- src/nippo.py
+++ src/nippo.py
@@ -3,30 +3,17 @@ import vim
 import os
 import sys
 import traceback
-from datetime import timedelta
+from datetime import timedelta as after
 from datetime import date
 
-nippo_runtime_path = vim.eval("g:nippo#runtime_path")
+try:
+    nippo_runtime_path = vim.eval("g:nippo#runtime_path")
+except NameError:
+    nippo_runtime_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(nippo_runtime_path)
 from config import jp as config
 
 class Nippo:
-
-    @staticmethod
-    def arg2daysago(arg):
-        if "today".startswith(arg):
-            return 0
-        elif "yesterday".startswith(arg):
-            return 1
-        else:
-            try:
-                return int(arg)
-            except ValueError:
-                print("invarid argument")
-
-    @staticmethod
-    def date_from(daysago):
-        return date.today() - timedelta(days=daysago)
 
     @staticmethod
     def extract_vim_error(error):
@@ -48,18 +35,28 @@ class Nippo:
             sys.stderr.write(Nippo.extract_vim_error(traceback.format_exc()))
             return
 
-        if not self.exists_nippo():
+        if len(vim.current.buffer) == 1 and vim.current.buffer[0] == "":
             vim.current.buffer[0] = self.nippo_title
+            for task in Nippo(self.nippo_date + after(days=-1)).tasks():
+                vim.current.buffer.append(task)
 
-    def __init__(self, arg="today"):
-        self.nippo_date = nippo_date = Nippo.date_from(daysago=Nippo.arg2daysago(arg))
-        self.day        = day        = f"{nippo_date.day}{config.day_suffix}"
-        self.month      = month      = f"{nippo_date.month}{config.month_suffix}"
-        self.year       = year       = f"{nippo_date.year}{config.year_suffix}"
-        self.nippo_dir  = nippo_dir  = os.path.join(config.nippo_home_directory, year, month)
-        nippo_name                   = f"{day}.md"
-        self.nippo_path              = os.path.join(nippo_dir, nippo_name)
-        self.nippo_title             = f"# {year}{month}{day}"
+    def lines(self):
+        with open(self.nippo_path) as nippo_file:
+            lines = nippo_file.readlines()
+        return lines
+
+    def tasks(self):
+        return [line for line in self.lines() if line.startswith("- [ ] ")]
+
+    def __init__(self, nippo_date="today"):
+        self.nippo_date  = nippo_date
+        self.day         = day        = f"{nippo_date.day}{config.day_suffix}"
+        self.month       = month      = f"{nippo_date.month}{config.month_suffix}"
+        self.year        = year       = f"{nippo_date.year}{config.year_suffix}"
+        self.nippo_dir   = nippo_dir  = os.path.join(config.nippo_home_directory, year, month)
+        nippo_name                    = f"{day}.md"
+        self.nippo_path               = os.path.join(nippo_dir, nippo_name)
+        self.nippo_title              = f"# {year}{month}{day}"
 
 # その日の日報を作成して表示
 # 昨日のやつを指定して見る
@@ -71,7 +68,9 @@ class Nippo:
 # print(config.nippo_open_error_message)
 # 1.5ヶ月前とか
 # 特定の日付とか
+# day_as_word
 
 # 型付け不可
 # os.mkdirs
 # xrange 
+# import できないんかいfrom src import
