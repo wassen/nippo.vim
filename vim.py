a31fdfd30c27c0af710e5be646e004c1f4be1c32
wassen <k-asayama@outlook.com>
2017-08-01 00:49:07 +0900

    add mock module for test

---
+++ vim.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# this is mock module
+
+def eval(string):
+    return "mock string"
+
+
+if __name__ == "__main__":
+    print('This "vim" object is for test.')
+
