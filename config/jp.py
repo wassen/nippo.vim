57ed9eedc4749a3e75b8fdfa112bc51c6c5c04cf
wassen <k-asayama@outlook.com>
2017-08-04 00:14:52 +0900

    fix: add mock and setting of home_directory

--- config/jp.py
+++ config/jp.py
@@ -6,7 +6,7 @@ month_suffix = "月"
 year_suffix = "年"
 nippo_open_error_message = "日報ファイルのオープンに失敗しました。"
 
-vim_nippo_home_directory = "nippo#directory"
+vim_nippo_home_directory = "nippo#home_directory"
 
 if vim_nippo_home_directory in vim.vars:
     nippo_directory = os.path.join(vim.eval(vim_nippo_home_directory), "nippo")
